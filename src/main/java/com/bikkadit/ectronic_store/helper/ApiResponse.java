f841937c607d8955edc540c30260335842fba12a
surajdonge123 <surajdonge143@gmail.com>
2023-07-07 15:56:40 +0530

    adding constants

---
+++ src/main/java/com/bikkadit/ectronic_store/helper/ApiResponse.java
@@ -0,0 +1,20 @@
+package com.bikkadit.ectronic_store.helper;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApiResponse {
+
+    private String message;
+    private boolean success;
+    private HttpStatus status;
+
+}
+
+
+
