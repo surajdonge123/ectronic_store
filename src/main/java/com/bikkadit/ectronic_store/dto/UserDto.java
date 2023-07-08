72ef1b7a9d5e7af875a2205e9703aa852b3533a2
surajdonge123 <surajdonge143@gmail.com>
2023-07-08 16:37:05 +0530

    Adding Validation

--- src/main/java/com/bikkadit/ectronic_store/dto/UserDto.java
+++ src/main/java/com/bikkadit/ectronic_store/dto/UserDto.java
@@ -2,6 +2,11 @@ package com.bikkadit.ectronic_store.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Builder
 @Getter
 @Setter
@@ -10,10 +15,27 @@ import lombok.*;
 public class UserDto {
 
     private String userId;
+
+    @NotBlank
+    @Size(max = 50,min = 3,message = "Invalid user Name!! Name should contain minimum 3 letters")
     private String name;
+
+    @Email
+    @Pattern(regexp = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$",message = "Invalid Email")
     private String email;
+
+    @NotBlank
+    @Size(min = 4,max = 10,message = "Invalid details")
     private String gender;
+
+    @NotBlank
+    @Size(min = 10,max = 255,message = "Invalid details !! describe more about yourself")
     private String about;
+
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d)[a-zA-Z\\\\d]{8,}$")
     private String password;
+
+
     private String imageName;
 }
