86c1bf1207a293bca583c1faad3a1c2dccbdbd95
surajdonge123 <surajdonge143@gmail.com>
2023-07-13 18:04:40 +0530

    Logic for File Implmentation

--- src/main/java/com/bikkadit/ectronic_store/exception/BadApiRequest.java
+++ src/main/java/com/bikkadit/ectronic_store/exception/BadApiRequest.java
@@ -1,4 +1,12 @@
 package com.bikkadit.ectronic_store.exception;
 
-public class BadApiRequest {
+public class BadApiRequest extends RuntimeException{
+
+    public BadApiRequest(String message){
+        super(message);
+    }
+    public BadApiRequest(){
+        super("Bad api request !!");
+    }
+
 }
