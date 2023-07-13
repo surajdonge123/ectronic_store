b68c551d5b00383996838bdbdb4b9ed9f5da90ca
surajdonge123 <surajdonge143@gmail.com>
2023-07-13 17:16:55 +0530

    writing method for upload file

--- src/main/java/com/bikkadit/ectronic_store/service/FileService.java
+++ src/main/java/com/bikkadit/ectronic_store/service/FileService.java
@@ -1,4 +1,12 @@
 package com.bikkadit.ectronic_store.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+
 public interface FileService {
+
+    String uploadFile(MultipartFile file,String path);
+    InputStream getResource(String path,String name);
+
 }
