02d5712f93b6f6870a762ed9f5c743ecb39e6566
surajdonge123 <surajdonge143@gmail.com>
2023-07-14 15:36:44 +0530

    Logic for File Implementation

--- src/main/java/com/bikkadit/ectronic_store/service/impl/FileImplementation.java
+++ src/main/java/com/bikkadit/ectronic_store/service/impl/FileImplementation.java
@@ -1,16 +1,13 @@
 package com.bikkadit.ectronic_store.service.impl;
-
 import com.bikkadit.ectronic_store.exception.BadApiRequest;
 import com.bikkadit.ectronic_store.service.FileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.InputStream;
 import java.util.UUID;
-
 @Service
 public class FileImplementation implements FileService {
 
@@ -31,8 +28,6 @@ public class FileImplementation implements FileService {
            throw new BadApiRequest("File with this "+extension+" is not allowed");
 
        }
-
-
         return null;
     }
 
