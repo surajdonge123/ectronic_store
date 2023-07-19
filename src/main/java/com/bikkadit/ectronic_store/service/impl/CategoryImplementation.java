0ab90fa5be8246186c7500011bfe45175a1f46fe
surajdonge123 <surajdonge143@gmail.com>
2023-07-19 14:17:47 +0530

    logic for createCategory added by suraj

--- src/main/java/com/bikkadit/ectronic_store/service/impl/CategoryImplementation.java
+++ src/main/java/com/bikkadit/ectronic_store/service/impl/CategoryImplementation.java
@@ -1,7 +1,10 @@
 package com.bikkadit.ectronic_store.service.impl;
 
+import com.bikkadit.ectronic_store.constant.AppConstant;
 import com.bikkadit.ectronic_store.dto.CategoryDto;
 import com.bikkadit.ectronic_store.dto.PageableResponse;
+import com.bikkadit.ectronic_store.entity.Category;
+import com.bikkadit.ectronic_store.exception.ResourceNotFoundException;
 import com.bikkadit.ectronic_store.repository.CategoryRepository;
 import com.bikkadit.ectronic_store.service.CategoryService;
 import org.modelmapper.ModelMapper;
@@ -20,13 +23,26 @@ public class CategoryImplementation implements CategoryService {
     @Override
     public CategoryDto createCategory(CategoryDto categoryDto) {
 
+        Category category = modelMapper.map(categoryDto, Category.class);
+        Category savedCategory = categoryRepository.save(category);
+        CategoryDto dto = modelMapper.map(savedCategory, CategoryDto.class);
+
         return null;
 
 
     }
 
     @Override
-    public CategoryDto updateCategory(CategoryDto categoryDto, String categoryId) {
+    public CategoryDto updateCategory(CategoryDto categoryDto, String categoryId)
+    {
+        /*Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException(AppConstant.USER_NOT_FOUND));
+        category.setTitle(categoryDto.getTitle());
+        category.setDescription(categoryDto.getDescription());
+        category.setCoverImage(categoryDto.getCoverImage());
+        Category updatedCategory = categoryRepository.save(category);
+        CategoryDto dto = modelMapper.map(updatedCategory, CategoryDto.class);
+        return dto;*/
+
         return null;
     }
 
