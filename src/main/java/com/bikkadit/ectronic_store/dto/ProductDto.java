f0905e1a8f453cfd7522ea0d950a4c1a09313d68
surajdonge123 <surajdonge143@gmail.com>
2023-07-21 13:09:39 +0530

    Validation added in product Dto class by suraj

--- src/main/java/com/bikkadit/ectronic_store/dto/ProductDto.java
+++ src/main/java/com/bikkadit/ectronic_store/dto/ProductDto.java
@@ -1,4 +1,51 @@
 package com.bikkadit.ectronic_store.dto;
 
+import lombok.*;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.Column;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductDto {
+
+    private String productId;
+    @NotBlank(message = "Product title should not be Blank")
+    @Size(min = 3,max = 255,message = "Product Title should contain at least 3 letters")
+    private String productTitle;
+    @NotBlank(message = "Brand Name Should not be blank")
+    @Size(min = 1,max = 25,message = "Brand Title should contains at least 1 letter and max 25 letters")
+    private String Brand;
+    @NotBlank(message = "Manufactured name should not be null !!")
+    @Size(min = 1,max = 25,message ="Manufacturer company name should contains at least one letters !!")
+    private String manufacturedBy;
+    @NotBlank(message = "Country name should not be null !!")
+    @Size(min = 1,max = 25,message ="Manufacturer country name should contains at least one letters !!")
+    private String manufacturedIn;
+    @NotBlank(message = "product Description should not be null")
+    @Size(min = 15,max = 100000,message = "Description of product should be min of 15 letters")
+    private String description;
+    private Date addedDate;
+    @NotBlank
+    @DecimalMin(value = "10",message = "price must be at least 10")
+    @DecimalMax(value = "1000000",message = "price must not  Exceed than 1000000")
+    private Long price;
+    private Integer discountedPrice;
+    @NotBlank(message = "Quantity should not null !!")
+    private Integer Quantity;
+    private boolean live;
+    private boolean stock;
+
+
+
+
+
 }
--- src/main/java/com/bikkadit/ectronic_store/entity/Product.java
+++ src/main/java/com/bikkadit/ectronic_store/entity/Product.java
@@ -20,6 +20,9 @@ public class Product {
     @Id
     private String productId;
     private String productTitle;
+    private String Brand;
+    private String manufacturedBy;
+    private String manufacturedIn;
     @Column(length = 10000)
     private String description;
     private Date addedDate;
@@ -28,5 +31,6 @@ public class Product {
     private Integer Quantity;
     private boolean live;
     private boolean stock;
+
 
 }
