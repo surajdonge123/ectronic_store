1d1236c70d9ac07c020b329156b290396bbe3a44
surajdonge123 <surajdonge143@gmail.com>
2023-07-21 11:27:51 +0530

    Fields added in entity Class by suraj

--- src/main/java/com/bikkadit/ectronic_store/entity/Product.java
+++ src/main/java/com/bikkadit/ectronic_store/entity/Product.java
@@ -1,7 +1,32 @@
 package com.bikkadit.ectronic_store.entity;
 
-import javax.persistence.Entity;
+import lombok.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
+@Table(name = "Products")
 public class Product {
+
+    @Id
+    private String productId;
+    private String productTitle;
+    @Column(length = 10000)
+    private String description;
+    private Date addedDate;
+    private Long price;
+    private Integer discountedPrice;
+    private Integer Quantity;
+    private boolean live;
+    private boolean stock;
+
 }
