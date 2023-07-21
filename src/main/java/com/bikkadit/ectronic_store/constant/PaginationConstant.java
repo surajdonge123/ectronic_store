becf9371d2bda88fda3579387d80932359b2f444
surajdonge123 <surajdonge143@gmail.com>
2023-07-21 10:38:44 +0530

    constants add in PaginationConstant  by suraj

---
+++ src/main/java/com/bikkadit/ectronic_store/constant/PaginationConstant.java
@@ -0,0 +1,16 @@
+package com.bikkadit.ectronic_store.constant;
+
+public class PaginationConstant {
+
+
+    public static final String PAGE_NUMBER="0";
+
+    public static final String PAGE_SIZE="10";
+
+    public static final String SORT_BY_USER="name";
+
+    public static final String SORT_DIR="asc";
+
+
+
+}
