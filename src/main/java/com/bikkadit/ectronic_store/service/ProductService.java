cb8f359de28e94930ab707dbab8efb5b4fc04ec0
surajdonge123 <surajdonge143@gmail.com>
2023-07-22 15:09:41 +0530

    adding methods to ProductService by suraj

--- src/main/java/com/bikkadit/ectronic_store/service/ProductService.java
+++ src/main/java/com/bikkadit/ectronic_store/service/ProductService.java
@@ -2,9 +2,23 @@ package com.bikkadit.ectronic_store.service;
 
 import com.bikkadit.ectronic_store.dto.ProductDto;
 
+import java.util.List;
+
 public interface ProductService {
 
-    /*public ProductDto createProduct();*/
+    public ProductDto createProduct();
+
+    public ProductDto updateProduct(ProductDto productDto,String proId);
+
+    public void deleteProduct(String proId);
+
+    public List<ProductDto> getAllProduct();
+
+    public ProductDto getSingleProduct(String proId);
+
+
+
+
 
 
 }
