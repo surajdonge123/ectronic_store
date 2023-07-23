ec2eb70b9280a6effd4ccad59567abced246b4ae
surajdonge123 <surajdonge143@gmail.com>
2023-07-23 14:33:16 +0530

    implements method from service to impl by suraj

--- src/main/java/com/bikkadit/ectronic_store/service/impl/ProductImplementation.java
+++ src/main/java/com/bikkadit/ectronic_store/service/impl/ProductImplementation.java
@@ -1,11 +1,47 @@
 package com.bikkadit.ectronic_store.service.impl;
 
+import com.bikkadit.ectronic_store.dto.ProductDto;
 import com.bikkadit.ectronic_store.service.ProductService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductImplementation implements ProductService {
 
 
+    @Override
+    public ProductDto createProduct(ProductDto productDto) {
+        return null;
+    }
 
+    @Override
+    public ProductDto updateProduct(ProductDto productDto, String proId) {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(String proId) {
+
+    }
+
+    @Override
+    public List<ProductDto> getAllProduct() {
+        return null;
+    }
+
+    @Override
+    public ProductDto getSingleProduct(String proId) {
+        return null;
+    }
+
+    @Override
+    public List<ProductDto> getByTitle(String subTitle) {
+        return null;
+    }
+
+    @Override
+    public List<ProductDto> getByLive() {
+        return null;
+    }
 }
