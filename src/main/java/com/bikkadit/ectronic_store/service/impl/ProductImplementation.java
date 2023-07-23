f1c289f7a064122f12bfe48eef689aae2fa10691
surajdonge123 <surajdonge143@gmail.com>
2023-07-23 14:31:33 +0530

    productImplementation class added by suraj

---
+++ src/main/java/com/bikkadit/ectronic_store/service/impl/ProductImplementation.java
@@ -0,0 +1,11 @@
+package com.bikkadit.ectronic_store.service.impl;
+
+import com.bikkadit.ectronic_store.service.ProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductImplementation implements ProductService {
+
+
+
+}
