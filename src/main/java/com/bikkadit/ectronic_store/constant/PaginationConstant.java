108bd5af2dc4a8839edb040c9570818dff77ccd2
surajdonge123 <surajdonge143@gmail.com>
2023-07-24 15:17:36 +0530

    PaginationConstant added for products by suraj

--- src/main/java/com/bikkadit/ectronic_store/constant/PaginationConstant.java
+++ src/main/java/com/bikkadit/ectronic_store/constant/PaginationConstant.java
@@ -9,6 +9,8 @@ public class PaginationConstant {
 
     public static final String SORT_BY_USER="name";
 
+    public static  final String SORT_BY_PRODUCT="productTitle";
+
     public static final String SORT_BY_CATEGORY="title";
 
     public static final String SORT_DIR="asc";
