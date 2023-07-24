ce92e169e9748abaa20af766df6d12608e484cb2
surajdonge123 <surajdonge143@gmail.com>
2023-07-24 15:16:47 +0530

    AppConstant added for products by suraj

--- src/main/java/com/bikkadit/ectronic_store/constant/AppConstant.java
+++ src/main/java/com/bikkadit/ectronic_store/constant/AppConstant.java
@@ -7,6 +7,8 @@ public class AppConstant {
     public static final String CATEGORY_NOT_FOUND = "category not found with the given Id";
     public static final String USER_DELETE="User delete Successfully !!";
 
+    public static final String PRODUCT_DELETE="Product has been deleted successfully";
+
     public static final String FILE_UPLOAD="File has been posted successfully";
 
     public static final String PRODUCT_NOT_FOUND="Product not found with given Id";
